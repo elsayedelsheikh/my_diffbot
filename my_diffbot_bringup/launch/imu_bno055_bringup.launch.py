4354b50c0c439a6e22112519bdd7bd7337aeeacb
ElSayed ElSheikh <elsayed.elsheikh97@gmail.com>
2026-01-21 11:38:11 +0200

    add pkg and cfg

---
+++ my_diffbot_bringup/launch/imu_bno055_bringup.launch.py
@@ -0,0 +1,23 @@
+import os
+from ament_index_python.packages import get_package_share_directory
+from launch import LaunchDescription
+from launch_ros.actions import Node
+def generate_launch_description():
+    ld = LaunchDescription()
+    config = os.path.join(
+        get_package_share_directory('my_diffbot_bringup'),
+        'config',
+        'imu_bno055.yaml'
+        )
+        
+    node=Node(
+        package = 'bno055',
+        executable = 'bno055',
+        parameters = [config],
+        remappings=[
+            ("/imu/imu", "/imu/data"),
+            ("/imu/imu_raw", "/imu/data_raw"),
+        ],
+    )
+    ld.add_action(node)
+    return ld
